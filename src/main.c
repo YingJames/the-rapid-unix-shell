9528119e923ae31d24b4fd4f1d41f5337efadca8
YingJames <jamesy148@usf.edu>
2024-01-28 22:19:13 -0500

    add parsing to split arguments in user input

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #define MAX_LINE 256 /* The maximum length command */
 
 void runShell();
+char **parseCmd(char cmdInput[MAX_LINE]);
 
 int main() {
     runShell();
@@ -13,12 +14,31 @@ int main() {
 
 void runShell() {
     char cmdInput[MAX_LINE];
+    char **args;
     while (strcmp(cmdInput, "exit") != 0) {
         printf("rush> ");
-        fgets(cmdInput, MAX_LINE, stdin);
-
-        // Remove trailing newline, if there is one
-        cmdInput[strcspn(cmdInput, "\n")] = '\0';
+        fgets(cmdInput, sizeof cmdInput, stdin);
+        args = parseCmd(cmdInput);
     }
+}
+
+char **parseCmd(char cmdInput[MAX_LINE]) {
+    // replace trailing newline with null
+    cmdInput[strcspn(cmdInput, "\n")] = '\0';
+    char delimiter[] = " \t";
+    char *tokenPtr;
+    char *savePtr;
+    char *result[MAX_LINE];
+    int i = 0;
+
+    tokenPtr = strtok_r(cmdInput, delimiter, &savePtr);
+    while (tokenPtr != NULL) {
+        result[i] = tokenPtr;
+        tokenPtr = strtok_r(NULL, delimiter, &savePtr);
+        i++;
+    }
+    result[i] = NULL;
+
+    return result;
 }
 
