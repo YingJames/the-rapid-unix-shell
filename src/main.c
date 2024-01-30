3fc6433e6d8368be4ad556e25eaaf7d258606fb8
YingJames <jamesy148@usf.edu>
2024-01-30 10:38:32 -0500

    edit parseCmd to modify command input rather than return a double pointer

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "exec_command.h"
-#include "builtin_commands.h"
 
-#define MAX_LINE 256 /* The maximum length command */
+#include "constants.h"
+#include "builtin_commands.h"
+#include "exec_command.h"
 
 void runShell();
-char **parseCmd(char cmdInput[MAX_LINE]);
+void parseCmd(int *argc, char cmdInput[MAX_LINE], char argv[MAX_ARGS][MAX_LINE]);
 
 int main() {
     runShell();
@@ -16,40 +16,38 @@ int main() {
 
 void runShell() {
     char cmdInput[MAX_LINE];
-    char **argv;
+    char argv[MAX_ARGS][MAX_LINE];
+    int argc;
 
     int pathc = 0;
-    char **pathv = path(0, NULL);
-    while (strcmp(cmdInput, "exit") != 0) {
-        
+    char pathv[MAX_PATHS][MAX_LINE];
+    while (1) {
         printf("rush> ");
+        fflush(stdout);
+
+        // read input and then parse it to get arguments
         fgets(cmdInput, sizeof cmdInput, stdin);
-        argv = parseCmd(cmdInput);
-        int argc = sizeof (argv) / sizeof (argv[0]);
+        parseCmd(&argc, cmdInput, argv);
 
-        execCommand(pathc, pathv, argc, argv);
-
-
+        // execute the appropriate command
+        execCommand(&pathc, pathv, argc, argv);
     }
 }
 
-char **parseCmd(char cmdInput[MAX_LINE]) {
+void parseCmd(int *argc, char cmdInput[MAX_LINE], char argv[MAX_ARGS][MAX_LINE]) {
     // replace trailing newline with null
     cmdInput[strcspn(cmdInput, "\n")] = '\0';
     char delimiter[] = " \t";
     char *tokenPtr;
     char *savePtr;
-    char *result[MAX_LINE];
     int i = 0;
 
     tokenPtr = strtok_r(cmdInput, delimiter, &savePtr);
     while (tokenPtr != NULL) {
-        result[i] = tokenPtr;
+        strcpy(argv[i], tokenPtr);
         tokenPtr = strtok_r(NULL, delimiter, &savePtr);
         i++;
     }
-    result[i] = NULL;
-
-    return result;
+    strcpy(argv[i], "\0");
+    *argc = i;
 }
-
