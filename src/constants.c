8851440eba19837eea8432c08492a6fd8be39d66
YingJames <jamesy148@usf.edu>
2024-01-30 09:52:59 -0500

    replace macros with constants

---
+++ src/constants.c
@@ -0,0 +1,6 @@
+#include <stddef.h>
+#include "constants.h"
+
+const size_t MAX_LINE = 256; /* The maximum length command */
+const size_t MAX_ARGS = 64; /* The maximum number of arguments */
+const size_t MAX_PATHS = 64; /* The maximum number of paths */
---
+++ src/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const size_t MAX_LINE; /* The maximum length command */
+extern const size_t MAX_ARGS; /* The maximum number of arguments */
+extern const size_t MAX_PATHS; /* The maximum number of paths */
+
+#endif // CONSTANTS_H
