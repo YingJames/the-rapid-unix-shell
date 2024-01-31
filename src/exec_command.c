c46014adc234a56f08d3b36e0a2ea062cc302517
YingJames <jamesy148@usf.edu>
2024-01-31 14:15:17 -0500

    implement execution of commands from paths

--- src/exec_command.c
+++ src/exec_command.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include "constants.h"
 #include "builtin_commands.h"
 #include "exec_command.h"
@@ -18,8 +19,35 @@ void execCommand(size_t *pathc, char pathv[MAX_PATHS][MAX_LINE], int argc, char 
         cdCmd(argc, argv);
     }
 
+    // commands outside of builtins
     else {
-        handleError();
-    }
+        char fullPath[MAX_LINE];
+        char *cmd = argv[0]; 
+        for (size_t i = 0; i < *pathc; i++) {
+            strcpy(fullPath, pathv[i]);
+            strcat(fullPath, "/");
+            strcat(fullPath, cmd);
+            if (access(fullPath, X_OK) == 0) {
+                char *argvPointers[MAX_ARGS+1];
+                for (int i = 0; i < argc; i++) {
+                    argvPointers[i] = argv[i];
+                }
+                argvPointers[argc] = NULL;
 
+                pid_t pid = fork();
+
+                // child process
+                if (pid == 0)
+                    execv(fullPath, argvPointers);
+                else {
+                    // parent process
+                    int status;
+                    waitpid(pid, &status, 0);
+                }
+            } else {
+                handleError();
+                break;
+            }
+        }
+    }
 }
