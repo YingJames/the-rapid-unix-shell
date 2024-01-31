a09c42bea6d98ed55d3be4b6acccc4c88ad0579f
YingJames <jamesy148@usf.edu>
2024-01-31 13:04:19 -0500

    implement error handling

--- src/builtin_commands.c
+++ src/builtin_commands.c
@@ -18,7 +18,11 @@ void pathCmd(size_t *pathc, char pathv[MAX_PATHS][MAX_LINE], size_t argc, char a
 
 void cdCmd(size_t argc, char argv[MAX_ARGS][MAX_LINE]) {
     if (argc == 2) {
-        chdir(argv[2]);
+        if (chdir(argv[1]) != 0) {
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message)); 
+            fflush(stdout);
+        }
     } else if (argc == 1 || argc > 2) {
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message)); 
