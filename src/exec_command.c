41bb7bbd03934c41ec218eb51e1d90ee1586a6db
YingJames <jamesy148@usf.edu>
2024-01-30 21:21:18 -0500

    return an error when shell does not recognize a command

--- src/exec_command.c
+++ src/exec_command.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "constants.h"
 #include "builtin_commands.h"
 #include "exec_command.h"
 
 void execCommand(size_t *pathc, char pathv[MAX_PATHS][MAX_LINE], int argc, char argv[MAX_ARGS][MAX_LINE]) {
-    if (argc == 0) {
-        return;
-    }
-
-    else if (strcmp(argv[0], "exit") == 0) {
+    if (strcmp(argv[0], "exit") == 0) {
         exitCmd(argc);
     }
 
     else if (strcmp(argv[0], "path") == 0) {
         pathCmd(pathc, pathv, argc, argv);
+    }
+
+    else {
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+        fflush(stdout);
     }
 
 }
