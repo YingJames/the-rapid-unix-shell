a105b6513998ed655b43712fc52ac0aeb80097e7
YingJames <jamesy148@usf.edu>
2024-02-12 17:12:03 -0500

    realloc pathv with pathCmd

--- src/builtin_commands.c
+++ src/builtin_commands.c
@@ -5,21 +5,24 @@
 #include "include/utils.h"
 #include "include/builtin_commands.h"
 
-void pathCmd(char **pathv, size_t argc, char **argv) {
+void pathCmd(char **pathv, int argc, char **argv) {
     if (argc == 1) {
-        free(pathv[0]);
-        pathv[0] = NULL;
+        int pathc = strArrLen(pathv);
+        for (int i = 0; i < pathc; i++)
+            memset(pathv[i], 0, strlen(pathv[i]));
     } else {
-        size_t argIndex = 0;
+        int argIndex = 0;
         for (argIndex = 1; argv[argIndex] != NULL; argIndex++) {
-            pathv[argIndex-1] = realloc(pathv[argIndex-1], (strlen(argv[argIndex]) + 1) * sizeof(char));
+            pathv[argIndex-1] = realloc(pathv[argIndex-1], (MAX_LINE * sizeof(char)));
             strcpy(pathv[argIndex-1], argv[argIndex]);
+            pathv[argIndex-1][strlen(argv[argIndex])] = '\0';
         }
+//        memset(pathv[argIndex-1], 0, strlen(pathv[argIndex-1]));
         pathv[argIndex-1] = NULL;
     }
 }
 
-void cdCmd(size_t argc, char **argv) {
+void cdCmd(int argc, char **argv) {
     if (argc == 2) {
         if (chdir(argv[1]) != 0) {
             handleError();
@@ -29,7 +32,7 @@ void cdCmd(size_t argc, char **argv) {
     }
 }
 
-void exitCmd(size_t argc) {
+void exitCmd(int argc) {
     if (argc == 1) {
         exit(0);
     } else {
