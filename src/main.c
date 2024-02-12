1f2dbf37073be9427db17182c48b49785d8e1c5d
YingJames <jamesy148@usf.edu>
2024-02-12 17:14:29 -0500

    implement built-in commands

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include "include/utils.h"
 #include "include/clean_string.h"
+#include "include/builtin_commands.h"
+#include "include/exec_command.h"
 
 
 int runShell();
@@ -30,29 +32,46 @@ int handleCmds(char **cmdLines, char **pathv) {
     cmdLine[1] = calloc(MAX_LINE, sizeof(char));
     cmdLine[2] = NULL;
 
-    // split by args
-    char **args = calloc(MAX_ARGS, sizeof(char *));
+    // split by argv
+    char **argv = calloc(MAX_ARGS, sizeof(char *));
     for (int j = 0; j < MAX_ARGS; j++) {
-        args[j] = calloc(MAX_LINE, sizeof(char));
+        argv[j] = calloc(MAX_LINE, sizeof(char));
     }
-    for (int i = 0; strcmp(cmdLines[i], "\0") != 0; i++) {
+
+    // iterate through all commands
+    for (int i = 0; cmdLines[i] != NULL; i++) {
         // split by redirection if any
         int validRedirect = splitString(cmdLine, cmdLines[i], ">");
         if (validRedirect == -1) return -1;
         char *cmd = cmdLine[0];
         char *outputFile = cmdLine[1];
 
-        int validArgs = splitString(args, cmd, " \t");
+        int validArgs = splitString(argv, cmd, " \t");
         if (validArgs == -1) continue;
+
+        int argc = strArrLen(argv);
+        // check for built-in commands
+        if (strcmp(argv[0], "path") == 0) {
+            pathCmd(pathv, argc, argv);
+            continue;
+        } else if (strcmp(argv[0], "cd") == 0) {
+            cdCmd(argc, argv);
+            continue;
+        } else if (strcmp(argv[0], "exit") == 0) {
+            exitCmd(argc);
+            continue;
+        } else {
+            handlePathCommand(pathv, argv);
+        }
     }
 
     // free memory
     for (int i = 0; i < 3; i++)
         free(cmdLine[i]);
     for (int i = 0; i < MAX_ARGS; i++)
-        free(args[i]);
+        free(argv[i]);
     free(cmdLine);
-    free(args);
+    free(argv);
     return 0;
 }
 
@@ -63,20 +82,21 @@ int runShell() {
     char cmdDelimiter[] = "&";
     char argsDelimiter[] = " \t";
 
-    // allocate memory for command strings
-    char **cmdLines = calloc(MAX_COMMANDS, sizeof(char *));
-    for (int i = 0; i < MAX_COMMANDS; i++) {
-        cmdLines[i] = calloc(MAX_LINE, sizeof(char));
-    }
-
     // allocate memory for pathv and default path
     char **pathv = calloc(MAX_PATHS, sizeof(char *));
     for (int i = 0; i < MAX_PATHS; i++) {
         pathv[i] = calloc(MAX_LINE, sizeof(char));
     }
     strcpy(pathv[0], "/bin");
+    pathv[1] = NULL;
 
     while (1) {
+        // allocate memory for command strings
+        char **cmdLines = calloc(MAX_COMMANDS, sizeof(char *));
+        for (int i = 0; i < MAX_COMMANDS; i++) {
+            cmdLines[i] = calloc(MAX_LINE, sizeof(char));
+        }
+
         printf("rush> ");
         fflush(stdout);
 
@@ -87,10 +107,8 @@ int runShell() {
         lineInput[strcspn(lineInput, "\n")] = '\0';
         trim(lineInput);
 
-        // check for exit command
-        if (strcmp(lineInput, "exit") == 0) break;
         // check for empty input
-        else if (strlen(lineInput) == 0) continue;
+        if (strlen(lineInput) == 0) continue;
 
         // split input into cmdLines for parallel commands
         int validMultiCmd = splitString(cmdLines, lineInput, cmdDelimiter);
@@ -99,15 +117,17 @@ int runShell() {
 //        int cmdLinesLen = strArrLen(cmdLines);
         int validCmd = handleCmds(cmdLines, pathv);
         if (validCmd == -1) continue;
+
+        // free memory
+        for (int i = 0; i < MAX_COMMANDS; i++)
+            free(cmdLines[i]);
+        free(cmdLines);
     }
 
-    // free memory
-    for (int i = 0; i < MAX_COMMANDS; i++)
-        free(cmdLines[i]);
     for (int i = 0; i < MAX_PATHS; i++)
         free(pathv[i]);
-    free(cmdLines);
     free(pathv);
+
     return 0;
 }
 
